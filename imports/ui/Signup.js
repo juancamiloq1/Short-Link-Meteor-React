8e03f6a151582806fe849959d572a47a25ca1ad7
Juan Camilo Quintero <juancamiloq1@gmail.com>
2017-09-14 10:14:56 -0500

    Ejemplo de como se usa State para crear un contador y botones +1 y -1

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,10 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Signup extends React.Component{
+    constructor(props){
+      super(props);
+      this.state = {
+        count: this.props.count || 0 //Asi se puede dar un valor por defecto (0) o pasarle el state a travez de props.
+      };
+    }
+    addOne(){ // Asi se escriben metodos completos, abajo en el boton se ve como se crean en el mismo boton.
+      this.setState({
+        count: this.state.count + 1
+      });
+    }
     render(){
       return(
         <div>
           <h1>Unete a Short Link App</h1>
+          <p>{this.state.count}</p>
+          <button onClick={() => {
+            this.setState({ count: this.state.count - 1 })
+          }}>-1</button> {/*Asi se escriben metodos en linea*/}
+          <button onClick={this.addOne.bind(this)}>+1</button>
           <Link to='/'>Ya tienes una cuenta?</Link>
         </div>
       );
