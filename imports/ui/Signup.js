ab674836953fa38a1deb8c98bc795cb7306c8684
Juan Camilo Quintero <juancamiloq1@gmail.com>
2017-09-14 10:54:16 -0500

    accounts-password, Inclusion en el Signup component

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Accounts } from 'meteor/accounts-base';
 
 export default class Signup extends React.Component{
     constructor(props){
@@ -11,18 +12,20 @@ export default class Signup extends React.Component{
     onSubmit(e){
       e.preventDefault(); 
 
-      this.setState({
-        error: 'Algo salío mal.'
+      let email = this.refs.email.value.trim();
+      let password = this.refs.password.value.trim();
+      Accounts.createUser({ email: email, password: password}, (err) => {   // lo mismo que solo escribir {email, password} por ES6.
+        console.log('Crear Cuenta callback', err);
       });
     }
     render(){
       return(
         <div>
           <h1>Unete a Short Link App</h1>
-          {this.state.error ? <p>{this.state.error}</p> : undefined }
+          { this.state.error ? <p>{this.state.error}</p> : undefined }  {/*Se muestra el error solo si existe*/}
           <form>
-            <input type='email' name='email' placeholder='Email'/>
-            <input type='password' name='password' placeholder='Password'/>
+            <input type='email' ref='email' name='email' placeholder='Email'/>
+            <input type='password' ref='password' name='password' placeholder='Password'/>
             <button onClick={this.onSubmit.bind(this)}>Crear Cuenta</button>
           </form>
           <Link to='/'>Ya tienes una cuenta?</Link>
