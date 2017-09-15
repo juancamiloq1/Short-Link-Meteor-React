191f1c800a8cd02646943a3e23c517cab2a0c264
Juan Camilo Quintero <juancamiloq1@gmail.com>
2017-09-15 18:39:56 -0500

    Redirecting Shortened Links

--- server/main.js
+++ server/main.js
@@ -1,10 +1,21 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
 
 import '../imports/api/users';   //Solo necesitamos que se ejecute (Validacion de users)
-import '../imports/api/links';
+import { Links } from '../imports/api/links';
 import '../imports/startup/simple-schema-config';
 
 Meteor.startup(() => {
-  // code to run on server at startup
-  
+  WebApp.connectHandlers.use( (req, res, next) => {
+    const _id = req.url.slice(1);
+    const link = Links.findOne({ _id: _id });
+
+    if (link){
+      res.statusCode = 302;
+      res.setHeader( 'Location',link.url );
+      res.end();
+    } else {
+      next();
+    }
+  });
 });
